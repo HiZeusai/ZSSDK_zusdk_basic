a36eda9dab6dd3d85962b7effca155dd9a41a3cb
1234 <a1234@Astons-MacBook-Pro.local>
2025-11-23 20:45:01 +0800

    优化SDK

--- Package.swift
+++ Package.swift
@@ -23,16 +23,6 @@ let package = Package(
             checksum: "b6bd37f7d9f934358d4f9f19e7c12ca95a432d8ddbf5479931dd30e53c598722"
         ),
         .binaryTarget(
-            name: "ZSSDK",
-            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.6/ZSSDK.xcframework.zip",
-            checksum: "ea15641ea51036e64c74b8a18d2d9c06e5451698c741901bd4c21b0433634b12"
-        ),
-        .binaryTarget(
-            name: "ZSCoreKit",
-            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.6/ZSCoreKit.xcframework.zip",
-            checksum: "e8a951b0a8eb203af442c7b28a6f59f5546d34eb38cdd117eeff92abd5e34732"
-        ),
-        .binaryTarget(
             name: "GApple",
             url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.6/GApple.xcframework.zip",
             checksum: "bd26ca1f782244e2c09e38fa1964247ddd18181f36492d751e41cf8587547166"
@@ -45,9 +35,7 @@ let package = Package(
         .target(
             name: "ZUSDKBasicWrapper",
             dependencies: [
-                "ZSSDK",
                 "ZUSDK",
-                "ZSCoreKit",
                 "GApple",
                 "GGameCenter",
                 "channel_zeus"
