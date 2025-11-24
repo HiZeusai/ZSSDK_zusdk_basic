76c3fadc4e498da37cc56b1e14f0467008dc053c
1234 <a1234@Astons-MacBook-Pro.local>
2025-11-24 15:15:53 +0800

    优化SDK

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZUSDK",
-            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.7/ZUSDK.xcframework.zip",
+            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.8/ZUSDK_2.1.8_fixbundle.zip",
             checksum: "800376a860a538e0837a081d347670d328cfb3800a33ad9fe66330118407a8af"
         ),
         .binaryTarget(
@@ -32,17 +32,17 @@ let package = Package(
         ),
         .binaryTarget(
             name: "channel_zeus",
-            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.7/channel_zeus.xcframework.zip",
+            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.8/channel_zeus_2.1.8_fixbundle.zip",
             checksum: "b6bd37f7d9f934358d4f9f19e7c12ca95a432d8ddbf5479931dd30e53c598722"
         ),
         .binaryTarget(
             name: "GApple",
-            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.7/GApple.xcframework.zip",
+            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.8/GApple_2.1.8_fixbundle.zip",
             checksum: "bd26ca1f782244e2c09e38fa1964247ddd18181f36492d751e41cf8587547166"
         ),
         .binaryTarget(
             name: "GGameCenter",
-            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.7/GGameCenter.xcframework.zip",
+            url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.8/GGameCenter_2.1.8_fixbundle.zip",
             checksum: "c3be7d42b3a577addf0ce453823d78587640144ae9c004259fbfc274f890488f"
         ),
         .target(
